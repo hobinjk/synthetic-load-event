a7cffe32b01bbd8502469e71b785e3eeed82283b
James Hobin <hobinjk@gmail.com>
2018-10-30 16:42:51 -0400

    Attempt to replicate complete event

--- synthetic-load-event.js
+++ synthetic-load-event.js
@@ -1,12 +1,28 @@
+// performance.timing.loadEventEnd is equivalent to complete
+// css load almost replicates "complete" but doesn't include time-to-recalculate
 console.log(performance.now());
 let start = Date.now();
+let perfStart = performance.now();
 document.addEventListener('readystatechange', function(event) {
   console.log(event.target.readyState, performance.now());
   if (event.target.readyState === 'complete') {
     setTimeout(function() {
-      console.log('loadEventEnd', performance.timing.loadEventEnd - start);
+      console.log('loadEventEnd', performance.timing.loadEventEnd - start + perfStart);
     }, 300);
   }
 });
 
-// performance.timing.loadEventEnd is equivalent to complete
+Array.from(document.querySelectorAll('script')).forEach(script => {
+  script.addEventListener('load', function() {
+    console.log('script load', performance.now());
+  });
+});
+
+setTimeout(() => {
+  Array.from(document.querySelectorAll('link')).forEach(link => {
+    let href = link.href;
+    link.addEventListener('load', function() {
+      console.log('link load', performance.now(), href);
+    });
+  });
+}, 0);
